64f429cd72a5a94fc3bbbd43b9815eeff984ad2a
SumedhDawadi <sumedh.dawadi101@gmail.com>
2023-01-06 17:55:47 +0545

    portfolio

---
+++ src/text/contentEN.js
@@ -0,0 +1,108 @@
+
+
+var contentEN = {
+  "aboutme" : {
+    "name" : "About me",
+    "content" : 
+<>
+<p>
+ Greetings ♥, I'm <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sumedhdawadi/">Sumedh</a>,<br/>
+ As a security analyst with 15+ months of experience in a Security Operation Center, I am passionate about Cyber Security and have a successful track record in Incident Response, Malware Analysis, Digital Forensics, and Network Penetration Testing.In addition to my technical
+skills, I am a highly analytical and detail-oriented individual with strong problem-solving abilities. I
+am confident in my ability to identify and mitigate potential security threats and to develop and
+implement effective security measures. I am excited to bring my skills and experience to a new
+opportunity and to continue learning and growing in the field of cyber security.
+</p>
+<p>
+<h3>EXPERIENCES:</h3>
+<b>CryptoGen Nepal Pvt. Ltd.</b>
+<ul>
+  <li><b>Role   :</b> Associate Defensive Security Analyst</li>
+  <li><b>Tenure :</b> 13th February 2022 - 16th September 2022</li>
+</ul>
+<b>Accomplishment</b>
+<ul>
+  <li>Hands-on with Enterprise and Open-Source <b>SIEM</b> Solutions. </li>
+  <li> Knowledge on SOC Analyst workflow and procedure.</li>
+  <li> Experience on Real-time SOC monitoring during Projects.</li>
+  <li> Experience on Practical implementation of Digital Forensics for Projects.</li>
+  <li> Experience on Pratical implementation of Incident Response for Projects.</li>
+</ul>  
+</p>
+<p>
+<h4>Muktinath Bikas Bank Limited.</h4>
+<ul>
+  <li><b>Role   :</b> Information Security Analyst.</li>
+  <li><b>Tenure :</b> 18th September 2022 - Current.</li>
+</ul>
+<b>Roles and Responsiblities:</b>
+<ul>
+  <li>Real Time SIEM Monitoring.</li>
+  <li> Conducting Internal Network Penetration Testing.</li>
+  <li> Conducting various POC's in timely manner.</li>
+</ul> 
+</p>
+
+<p>
+I am excited to bring my skills and experience to a new opportunity and to continue learning and growing in the field of cyber security.
+</p>
+</>
+  },
+
+
+  "skills" : {
+    "name" : "Skills", 
+    "content" : 
+<>
+<p>
+<p>
+A Vulnerability Assessment (VA) tool written in bash script <a rel="noreferrer" target="_blank" href="https://github.com/SumedhDawadi/Reconme">ReconMe</a> can be useful for information gathering and bug hunting, as well as for managing the attack surface of a computer system or network. This type of script can automate the process of identifying and assessing vulnerabilities, making it an efficient and effective tool for VA.
+</p>
+<b>Here are few of my skills that I'm confdent about: </b>
+<ul>
+  <li> SOC (Security Operations Center).</li>
+  <li>SIEM (Security Information and Event Management).</li>
+  <li> Network Penetration Testing.</li>
+  <li>Incident Response (IR).</li>
+  <li>Malware Analysis.</li>
+</ul> 
+<b>Few of the Certificates that I own so far : </b>
+</p>
+<ul>
+
+    <li><a rel="noreferrer" target="_blank" href="https://codered.eccouncil.org/certificate/25371246-43f3-4a04-9e85-66a97fe46d65?logged=false">Ethical Hacking Essentials.</a>  </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1wxlIrvlfP1vWMdHxXhBMjGiyt57v95Wo/view">Network Defense Essentials.</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1aHDWL-8f83ECYQzj2-DXSomT_F82d1gd/view">NSE 1 Network Security Associate.</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1RR6HaaVnfZXsCy1pHDaYvdLIm4NM4QNz/view">Introduction to Vulnerability Management.</a></li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1Nn6jogUCjGB3ZqL72BnhEsWnwrpe08-X/view">Introduction to Dark Web Operations.</a></li>
+</ul>
+<h4>Knowledges : </h4>
+<ul>
+  <li>Capture The Flag (CTF).</li>
+  <li>Bash Scriptig for building security tools.</li>
+  <li>Attack Simulation.</li>
+  <li>Malware Analysis.</li>
+  <li><a rel="noreferrer" target="_blank" href="https://sumedh00.medium.com/">Research.</a></li>
+</ul>
+
+
+</>
+  },
+
+
+  "contact" : {
+    "name" : "Contact",
+    "content" : 
+<>
+<p>I am always open to discussing opportunities and am eager to contribute my skills and abilities to a team. Please feel free to contact me at your convenience.</p>
+
+<p> 📌 <a rel="noreferrer" target="_blank" href="https://github.com/SumedhDawadi">Github</a></p>
+<p> 💡 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sumedhdawadi/">Linkedin</a></p>
+<p> ✎ <a rel="noreferrer" target="_blank" href="https://medium.com/@sumedh00">Medium</a></p>
+<p> 📨 <a href="mailto:pingsumedh@gmail.com">pingsumedh@gmail.com</a></p>
+</>
+  }
+}
+
+
+export default contentEN
