1b63675bc6e2213937f08d30ea5fd5d2d11c3e6c
SumedhDawadi <sumedhdawadi101@gmail.com>
2023-06-17 21:25:24 +0545

    Updated

--- src/text/contentEN.js
+++ src/text/contentEN.js
@@ -6,7 +6,7 @@ var contentEN = {
     "content" : 
 <>
 <p>
- Greetings ♥, I'm <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sumedhdawadi/">Sumedh</a>,<br/>
+ Greetings ♥ I'm <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sumedhdawadi/">Sumedh</a>,<br/>
  As a security analyst with 15+ months of experience in a Security Operation Center, I am passionate about Cyber Security and have a successful track record in Incident Response, Malware Analysis, Digital Forensics, and Network Penetration Testing.In addition to my technical
 skills, I am a highly analytical and detail-oriented individual with strong problem-solving abilities. I
 am confident in my ability to identify and mitigate potential security threats and to develop and
@@ -38,8 +38,18 @@ opportunity and to continue learning and growing in the field of cyber security.
 <b>Roles and Responsiblities:</b>
 <ul>
   <li>Real Time SIEM Monitoring.</li>
-  <li> Conducting Internal Network Penetration Testing.</li>
-  <li> Conducting various POC's in timely manner.</li>
+  <li>Scanning and monitoring computers network systems and applications for security vulnerabilities to anticipate and protect against potential threats. </li>
+  <li>Investigating security breaches and other cybersecurity incident. </li>
+  <li>Deploying security measures.</li>
+  <li>Perform Vulnerability Assessment and Penetration Testing.</li>
+  <li>Analyzing IT requirements and providing objective advice on the use of IT security requirement. </li>
+  <li>Conducting internal security assessment </li>
+  <li>Conducting POC's in timely manner. </li>
+  <li>Gather feedback from end-user to continue improving systems. </li>
+  <li>Regular briefing of the task to the Line Manager</li>
+
+
+
 </ul> 
 </p>
 
@@ -58,7 +68,7 @@ I am excited to bring my skills and experience to a new opportunity and to conti
 <p>
 A Vulnerability Assessment (VA) tool written in bash script <a rel="noreferrer" target="_blank" href="https://github.com/SumedhDawadi/Reconme">ReconMe</a> can be useful for information gathering and bug hunting, as well as for managing the attack surface of a computer system or network. This type of script can automate the process of identifying and assessing vulnerabilities, making it an efficient and effective tool for VA.
 </p>
-<b>Here are few of my skills that I'm confident about: </b>
+<b>SKILLS: </b>
 <ul>
   <li> SOC (Security Operations Center).</li>
   <li>SIEM (Security Information and Event Management).</li>
@@ -66,29 +76,46 @@ A Vulnerability Assessment (VA) tool written in bash script <a rel="noreferrer" 
   <li>Incident Response (IR).</li>
   <li>Malware Analysis.</li>
 </ul> 
-<b>Few of the Certificates that I own so far : </b>
 </p>
-<ul>
 
-    <li><a rel="noreferrer" target="_blank" href="https://codered.eccouncil.org/certificate/25371246-43f3-4a04-9e85-66a97fe46d65?logged=false">Ethical Hacking Essentials.</a>  </li>
-    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1wxlIrvlfP1vWMdHxXhBMjGiyt57v95Wo/view">Network Defense Essentials.</a> </li>
-    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1aHDWL-8f83ECYQzj2-DXSomT_F82d1gd/view">NSE 1 Network Security Associate.</a> </li>
-    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1RR6HaaVnfZXsCy1pHDaYvdLIm4NM4QNz/view">Introduction to Vulnerability Management.</a></li>
-    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1Nn6jogUCjGB3ZqL72BnhEsWnwrpe08-X/view">Introduction to Dark Web Operations.</a></li>
-</ul>
-<h4>Knowledges : </h4>
+<h4>KNOWLEDGES: </h4>
 <ul>
   <li>Capture The Flag (CTF).</li>
   <li>Bash Scriptig for building security tools.</li>
   <li>Attack Simulation.</li>
   <li>Malware Analysis.</li>
+  <li>Bug Hunting.</li>
   <li><a rel="noreferrer" target="_blank" href="https://sumedh00.medium.com/">Research.</a></li>
 </ul>
 
 
 </>
   },
+  "achievementt" : {
+    "name" : "Achievement",
+    "content" : 
+<>
+<b>CERTIFICATES:</b>
+<ul>
 
+    <li><a rel="noreferrer" target="_blank" href="https://codered.eccouncil.org/certificate/25371246-43f3-4a04-9e85-66a97fe46d65?logged=false">Ethical Hacking Essentials.</a>  </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1wxlIrvlfP1vWMdHxXhBMjGiyt57v95Wo/view">Network Defense Essentials.</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1BMKjuHLhD2gV3TTJUILM4nXl7QH91hc7/view?pli=1">Practical Web Application Security and Testing</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1U6ukRzEjBz0PG3mq1gK-B2hR5AKz46I4/view">Breach and Attack Simulation</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1U6ukRzEjBz0PG3mq1gK-B2hR5AKz46I4/view">Cysa+ (CS0-002) By Cybrary</a> </li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1RR6HaaVnfZXsCy1pHDaYvdLIm4NM4QNz/view">Introduction to Vulnerability Management.</a></li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1Nn6jogUCjGB3ZqL72BnhEsWnwrpe08-X/view">Introduction to Dark Web Operations.</a></li>
+    <li><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1aHDWL-8f83ECYQzj2-DXSomT_F82d1gd/view">NSE 1 Network Security Associate.</a> </li>
+    
+</ul>
+<b>HALL OF FAME : </b>
+<ul>
+  <li><a rel="noreferrer" target="_blank" href="https://support.apple.com/en-us/HT201536">Acknowledged by Apple Inc.</a></li>
+  <li><a rel="noreferrer" target="_blank" href="https://www.surf.nl/en/hall-of-fame-0">Acknowledge by Surf.nl.</a></li>
+  <li><a rel="noreferrer" target="_blank" href="">Acknowledge by Softwarica College of IT and E-commerce.</a></li>
+</ul>
+</>
+  },
 
   "contact" : {
     "name" : "Contact",
